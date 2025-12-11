35caa5e9eb0215148ea48c282dbba6388e773ec5
NicolNonga <nicodemosarmindo@gmail.com>
2025-12-11 16:26:11 +0100

    Initial Commit

---
+++ MySound/MySoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySoundApp.swift
+//  MySound
+//
+//  Created by apple on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MySoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
